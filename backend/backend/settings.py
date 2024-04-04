5c2bf326c2def8ee4b3f766a9faf44bb0f220ebd
Kobelev Andrey <heiskobe@yandex.ru>
2024-04-04 22:42:01 +0300

    Add Actions3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,5 +126,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
-
-
